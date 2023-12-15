e7a0cc007d292c105363f00929b09dcacadd7ab0
Dario Becchio <dabek1986@gmail.com>
2023-12-15 10:11:21 +0100

    create array

---
+++ main.js
@@ -0,0 +1,28 @@
+/*
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede
+*/
+
+//Creare l’array di oggetti con le informazioni fornite.
+
+const memberEl = [
+    {
+        Name: 'Wayne Barnett',
+        Role: 'Founder & CEO',
+        Image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        Name: 'Angela Caroll',
+        Role: 'Chief Editor',
+        Image: 'angela-caroll-chief-editor.jpg'
+    }
+]
+
